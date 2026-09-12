f3ef4c7346fe87a0ef9eaf98e8d0c9478edcd254
Andreev Vitaly <junkers.22@mail.ru>
2026-09-12 22:40:05 +0300

    Configure project static assets

--- config/settings.py
+++ config/settings.py
@@ -91,6 +91,7 @@ TIME_ZONE = "Europe/Moscow"
 USE_I18N = True
 USE_TZ = True
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "media/"
